863d0c892c4bb9ab5e8a5447692db3dfc5dd88e4
Zubaidov <shohruz.zubaidov@gmail.com>
2023-01-20 17:16:39 +0400

    backup

--- myapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShuhratConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'shuhrat'
--- myapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('aboutus/', views.aboutus, name='aboutus')
-]
